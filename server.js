f0d9bd3c69cf139eb09c9c8fbb41745cc13e6928
kateaeryn <kate.ae.christensen@gmail.com>
2024-01-20 17:36:12 -0700

    mvc setup

--- server.js
+++ server.js
@@ -24,9 +24,8 @@ app.set("layout", "./layouts/layout")
 app.use(static)
 
 //index route
-app.get("/", function (req, res) {
-  res.render("index", {title: "Home"})
-})
+app.get("/", baseController.buildHome)
+
 
 /* ***********************
  * Local Server Information
