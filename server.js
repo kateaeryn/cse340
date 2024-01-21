802499500ffcadf0623d9c10457b5352f69f403f
kateaeryn <kate.ae.christensen@gmail.com>
2024-01-20 19:42:31 -0700

    fixed?

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
 const baseController = require("./controllers/baseController")
+const inventoryRoute = require("./routes/inventoryRoute")
 
 /* ***********************
  * View Engine and Templates
@@ -27,6 +28,8 @@ app.use(static)
 //index route
 app.get("/", baseController.buildHome)
 
+// Inventory routes
+app.use("/inv", inventoryRoute)
 
 /* ***********************
  * Local Server Information
